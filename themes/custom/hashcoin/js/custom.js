01464f46dd92879ebc41e6180c43e06783cd2277
Mangesh <mangesh.gawade@datamatics.com>
2019-05-29 21:23:15 +0530

    Changes for testimonial and news blocks

---
+++ themes/custom/hashcoin/js/custom.js
@@ -0,0 +1,27 @@
+(function ($) {
+  $(document).ready(function () {
+    //Js code for add review stars according to the enter numbers out of five.
+    var stars = '<span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>';
+    $(stars).insertAfter(".views-field-field-review-numbers > div");
+
+    $('.views-field-field-review-numbers').each(function () {
+      var review_number = $(this).children('.field-content').text();
+
+      if (review_number == 1) {
+        $(this).children('.fa:lt(1)').addClass('checked');
+      }
+      else if (review_number == 2) {
+        $(this).children('.fa:lt(2)').addClass('checked');
+      }
+      else if (review_number == 3) {
+        $(this).children('.fa:lt(3)').addClass('checked');
+      }
+      else if (review_number == 4) {
+        $(this).children('.fa:lt(4)').addClass('checked');
+      }
+      else if (review_number == 5) {
+        $(this).children('.fa:lt(5)').addClass('checked');
+      }
+    });
+  });
+})(jQuery);
